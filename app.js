bdb3e075cf48f1ff77977adc75354930d5401f8a
davemachado-o <davidmachadodj@gmail.com>
2022-04-30 04:54:48 -0300

    rutas app.js actualizadas + register.html base

--- app.js
+++ app.js
@@ -29,7 +29,15 @@ app.get("/register", (req, res) => {
     res.sendFile(path.resolve(__dirname, "views/register.html"))
 })
 
+app.post("/register", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "views/register.html"))
+})
+
 app.get("/login", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "views/login.html"))
+})
+
+app.post("/login", (req, res) => {
     res.sendFile(path.resolve(__dirname, "views/login.html"))
 })
 
