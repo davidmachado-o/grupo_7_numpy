4f36bd1b763fa85d1502a4cef9fefca4bbca4896
davidmachado-o <davidmachadodj@gmail.com>
2022-07-14 15:22:12 -0300

    completando modelos

---
+++ database/models/Product.js
@@ -0,0 +1,64 @@
+module.exports = (sequelize, DataTypes) => {
+    let alias = 'Products';
+    let cols = {
+        id: {
+            type: DataTypes.INTEGER,
+            primaryKey: true,
+            autoIncrement: true,
+            },
+        name: {
+            type: DataTypes.STRING(45),
+            allowNull: false,
+            },
+        model: {
+            type: DataTypes.STRING(45),
+            allowNull: true,
+            },
+        processor: {
+            type: DataTypes.STRING(45),
+            allowNull: true,
+            },
+        memory: {
+            type: DataTypes.STRING(45),
+            allowNull: true,
+            },
+        storage: {
+            type: DataTypes.STRING(45),
+            allowNull: true,
+            },
+        price_current: {
+            type: DataTypes.STRING(45),
+            allowNull: false,
+            },
+        price_discount: {
+            type: DataTypes.STRING(45),
+            allowNull: false,
+            },
+        stock: {
+            type: DataTypes.STRING(11),
+            allowNull: false,
+            },
+        description: {
+            type: DataTypes.STRING(100),
+            allowNull: false,
+            },
+        created_date: {
+            type: DataTypes.DATE,
+            defaultValue: DataTypes.NOW,
+            allowNull: false,
+            },
+        updated_date: {
+            type: DataTypes.DATE,
+            defaultValue: DataTypes.NOW,
+            allowNull: false,
+        }
+        }
+    
+    let config = {
+        tableName: 'products',
+        timestamps: true,
+        underscored: true
+    };
+    
+    const Product = sequelize.define(alias, cols, config);
+}
---
+++ database/models/User.js
@@ -0,0 +1,35 @@
+module.exports = (sequelize, DataTypes) => {
+    let alias = 'Users';
+    let cols = {
+        id: {
+            type: DataTypes.INTEGER(11),
+            allowNull: false,
+            primaryKey: true,
+            autoIncrement: true
+        },
+        name: {
+            type: DataTypes.STRING(45),
+            allowNull: false,
+        },
+        last_name: {
+            type: DataTypes.STRING(45),
+            allowNull: false,
+        },
+        email: {
+            type: DataTypes.STRING(45),
+            allowNull: false,
+        },
+        password: {
+            type: DataTypes.STRING(45),
+            allowNull: false,
+        }
+    };
+
+    let config = {
+        tableName: 'users',
+        timestamps: true,
+        underscored: true
+    };
+
+    const User = sequelize.define(alias, cols, config);
+}
