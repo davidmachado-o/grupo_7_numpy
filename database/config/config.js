9f0124dd7cb376ece0d46d0add3444bedc3c6953
mechita87 <mercedes.cavanagh18@gmail.com>
2022-08-08 08:47:27 -0300

    Fotos

--- database/config/config.js
+++ database/config/config.js
@@ -1,7 +1,7 @@
 module.exports = {
   "development": {
     "username": "root",
-    "password": null,
+    "password": "root",
     "database": "numpy_db",
     "host": "127.0.0.1",
     "dialect": "mysql",
@@ -12,14 +12,14 @@ module.exports = {
   },
   "test": {
     "username": "root",
-    "password": null,
+    "password": "root",
     "database": "numpy_db",
     "host": "127.0.0.1",
     "dialect": "mysql"
   },
   "production": {
     "username": "root",
-    "password": null,
+    "password": "root",
     "database": "numpy_db",
     "host": "127.0.0.1",
     "dialect": "mysql"
