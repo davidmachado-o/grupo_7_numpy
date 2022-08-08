27c37b50d41aa959d052c845986232b64d6ba265
mechita87 <mercedes.cavanagh18@gmail.com>
2022-08-08 15:17:21 -0300

    Validando formulario

--- public/js/productAdd.js
+++ public/js/productAdd.js
@@ -1,8 +1,11 @@
 const formulario = document.getElementById('productAddForm');
 const inputs = document.querySelectorAll('#productAddForm input');
+const texto = document.querySelectorAll('#productAddForm textarea');
+
 
 const expresiones = {
-	name: /^[a-zA-Z0-9\_\-]{5,16}$/ // Letras, numeros, guion y guion_bajo
+    name: /^[a-zA-Z0-9\_\-]{5,16}$/, // Letras, numeros, guion y guion_bajo
+    description: /^[a-zA-ZÀ-ÿ\s]{20,1000}$/
 }
 
 const validarFormulario = (e) => {
@@ -17,13 +20,34 @@ const validarFormulario = (e) => {
                 document.getElementById('grupo__name').classList.remove('formulario__grupo-correcto');
                 document.querySelector('#grupo__name .formulario__input-error').classList.add('formulario__input-error-activo')
             }
-        break;
+        break
+
+}}
+
+const validarFormulario2 = (e) => {
+    switch (e.target.name){
+        case "description":
+            if (expresiones.description.test(e.target.value)){
+                document.getElementById('grupo__description').classList.remove('formulario__grupo-incorrecto');
+                document.getElementById('grupo__description').classList.add('formulario__grupo-correcto');
+                document.querySelector('#grupo__description .formulario__input-error').classList.remove('formulario__input-error-activo')
+            }else{
+                document.getElementById('grupo__description').classList.add('formulario__grupo-incorrecto')
+                document.getElementById('grupo__description').classList.remove('formulario__grupo-correcto');
+                document.querySelector('#grupo__description .formulario__input-error').classList.add('formulario__input-error-activo')
+            }
+        break
     }
 }
 
 
 inputs.forEach((input) => {
-	input.addEventListener('keyup', validarFormulario);
-	input.addEventListener('blur', validarFormulario);
+    input.addEventListener('keyup', validarFormulario);
+    input.addEventListener('blur', validarFormulario);
+});
+
+texto.forEach((textarea) => {
+    textarea.addEventListener('keyup', validarFormulario2);
+    textarea.addEventListener('blur', validarFormulario2);
 });
 
