c883fc3294b775ff3489d5924f24e5b5cff93a50
mechita87 <mercedes.cavanagh18@gmail.com>
2022-08-09 14:14:23 -0300

    algo

--- database/config/config.js
+++ database/config/config.js
@@ -6,20 +6,20 @@ module.exports = {
     "host": "127.0.0.1",
     "dialect": "mysql",
     //Config para Mac
-   "dialectOptions": {
-    "socketPath": "/Applications/MAMP/tmp/mysql/mysql.sock"
-     }
+   //"dialectOptions": {
+    //"socketPath": "/Applications/MAMP/tmp/mysql/mysql.sock"
+     //}
   },
   "test": {
     "username": "root",
-    "password": "root",
+    "password": null,
     "database": "numpy_db",
     "host": "127.0.0.1",
     "dialect": "mysql"
   },
   "production": {
     "username": "root",
-    "password": "root",
+    "password": null,
     "database": "numpy_db",
     "host": "127.0.0.1",
     "dialect": "mysql"
