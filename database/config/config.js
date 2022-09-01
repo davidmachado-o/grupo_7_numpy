075351866f20df9bbcc1d654d45530af475df9f5
davidmachado-o <davidmachadodj@gmail.com>
2022-09-01 04:23:16 -0300

    estilizando y mejorando responsive

--- database/config/config.js
+++ database/config/config.js
@@ -1,14 +1,14 @@
 module.exports = {
   "development": {
     "username": "root",
-    "password": "root",
+    "password": "",
     "database": "numpy_db",
     "host": "127.0.0.1",
     "dialect": "mysql",
     // Config para Mac
-  "dialectOptions": {
-   "socketPath": "/Applications/MAMP/tmp/mysql/mysql.sock"
-     }
+  // "dialectOptions": {
+  //  "socketPath": "/Applications/MAMP/tmp/mysql/mysql.sock"
+  //    }
   },
   "test": {
     "username": "root",
