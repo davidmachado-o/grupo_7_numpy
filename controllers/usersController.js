e2281ed2801089ac03ee9f15321885750b0a9593
ale-collazo <ale_collazo02@hotmail.com>
2022-09-01 14:27:52 -0300

    Agregado detalles en productDetail, y UsersController

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -81,96 +81,48 @@ const usersController = {
 
 
     processRegister: async (req, res) => {
+
         const resultValidation = validationResult(req);
 
-        if (resultValidation.errors.length > 0) {
-            return res.render('register', {
-                errors: resultValidation.mapped(),
-                oldData: req.body
-            });
+        if (resultValidation.isEmpty()) {
+            //Verifico si el email que ingreso ya fue registrado
+            let user = await db.User.findOne({
+                where: { email: req.body.email }
+            })
+                .catch(error => res.send(error));
+            if (user) {
+                resultValidation.errors.push({ msg: 'Este email ya está registrado.', param: 'email' })
+            }
+            //Verifico si escribio bien la contraseña
+            if (req.body.password != req.body.password_confirm) {
+                resultValidation.errors.push({ msg: 'La contraseña no coincide.', param: 'password' })
+            }
+            //Pregunto si hubo errores
+            if (resultValidation.errors.length > 0) {
+                return res.render('register', {
+                    errors: resultValidation.mapped(),
+                    oldData: req.body
+                });
+
+            }
+            //Registro la cuenta
+            try {
+                await db.User.create({
+                    users_products_id: 1,
+                    user_type_id: 1,
+                    name: req.body.name,
+                    last_name: ' ',
+                    email: req.body.email,
+                    password: bycrypt.hashSync(req.body.password, 10),
+                    picture_id: req.file ? req.file.filename : 'default.jpg' //cambiar aca INT(11) por VARCHAR(45) para que pueda ir a la base de datos Y tambien cambiarlo en models.
+                })
+                res.redirect('login')
+            } catch (error) {
+                console.log(error);
+            }
         }
-        //Registro la cuenta
-        try {await db.User.create({
-            users_products_id: 1,
-            user_type_id: 1,
-            name: req.body.name,
-            last_name: ' ',
-            email: req.body.email,
-            password: bycrypt.hashSync(req.body.password, 10),
-            picture_id: req.file ? req.file.filename : 'default.jpg' //cambiar aca INT(11) por VARCHAR(45) para que pueda ir a la base de datos Y tambien cambiarlo en models.
-        })
-        res.redirect('login')
-    }catch(error)
-    {
-        console.log(error);
     }
-}}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// let errors = validationResult(req);
-
-// if(req.file && req.file.filename.search(/jpg$|jpeg$|png$/m) == -1){
-//     errors.errors.push({msg: 'Solo formatos JPG, JPEG o PNG.', param:'img'});
-// }
-// if (errors.isEmpty()) {
-//     //Verifico si el email que ingreso ya fue registrado
-//     let user = await db.User.findOne({
-//         where: { email: req.body.email }
-//     })
-//         .catch(error => res.send(error));
-//     if (user) {
-//         errors.errors.push({msg: 'Este email ya está registrado.', param:'email'})
-//     }
-//     //Verifico si escribio bien la contraseña
-//     if(req.body.password != req.body.confirm_password){
-//         errors.errors.push({msg: 'La contraseña no coincide.', param:'password'})
-//     }
-//     //Pregunto si hubo errores
-//     if(errors.errors.length > 0){
-//         res.render('register', {
-//             error: errors.mapped(),
-//             old: req.body
-//         });
-//     }
-//Registro la cuenta
-//         await db.User.create({
-//             users_products_id: ' ',
-//             user_type_id: ' ',
-//             name: req.body.name,
-//             last_name: ' ',
-//             email: req.body.email,
-//             password: bycrypt.hashSync(req.body.password, 10),
-//             picture_id: req.file ? req.file.filename : 'default.jpg' //cambiar aca INT(11) por VARCHAR(45) por para que pueda ir a la base de datos Y tambien cambiarlo en models.
-//         })
-//             .then(res.redirect('login'))
-//             .catch(error => res.send(error));
-//     }
-//         res.render('register', {
-//         error: errors.mapped(),
-//         old: req.body
-//     });
-// }
-// }
+}
 
 module.exports = usersController;
 
